c82bb77b3c29b4b35cc923afa132404b447f6fed
AniaH <anna.jolanta.hoffmann@gmail.com>
2023-03-09 17:21:04 +0100

    komentarz

--- src/main/java/eu/chrost/Danger.java
+++ src/main/java/eu/chrost/Danger.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Danger {
     public static void doSomething() throws IOException {
-        throw new IOException("Coś nie pykło");
+        throw new IOException("Coś nie pykło, a może będzie OK");
         //throw new IllegalStateException("Jak do tego doszło - nie wiem");
     }
 }
